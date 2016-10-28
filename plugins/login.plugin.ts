0edc54202d241bf8508fd35d07a07dca03494846
riccardoscalet <riccardo.scalet@gmail.com>
2016-10-28 14:36:07 +0200

    Implementing basic auth.

--- plugins/login.plugin.ts
+++ plugins/login.plugin.ts
@@ -7,6 +7,7 @@ import {
 import * as Joi from "joi";
 
 const levelup = require("levelup");
+const auth = require('hapi-auth-basic');
 
 export class LoginPlugin extends Plugin {
 
@@ -18,51 +19,57 @@ export class LoginPlugin extends Plugin {
     }
 
     _register(server, options) {
+        server.register(auth, function (err) {
+            if (err) throw err;
+        });
+
+        server.auth.strategy("simpleAuthentication", "basic", {
+            validateFunc: $.proxy(this.login, this)
+        });
+
         server.route({
             method: 'POST',
             path: '/login',
-            handler: this.login,
             config: {
                 validate: {
                     payload: {
                         username: Joi.string().required(),
                         password: Joi.string().required()
                     }
+                },
+                auth: "simpleAuthentication",
+                handler: function (request, reply) {
+                    reply(request.auth.credentials.name);
+                }
+            }
+        })
+
+        server.route({
+            method: 'POST',
+            path: '/logout',
+            config: {
+                handler: function (request, reply) {
+                    request.auth.session.clear();
+                    return reply("Logout Successful.");
                 }
             }
         })
     }
 
-    login(request, reply) {
-        let credentials = {
-            username: request.payload.username,
-            password: request.payload.password
-        };
+    login(request, username, password, callback) {
+        // let credentials = {
+        //     username: request.payload.username,
+        //     password: request.payload.password
+        // };
 
-        this.usersService.get(credentials.username, function (err, value) {
+        this.usersService.get(username, function (err, value) {
             if (err) {
-                if (err.notFound) {
-                    return reply({
-                        result: 2,
-                        message: "User does not exist."
-                    });
-                }
-
-                return reply({
-                    result: -1,
-                    message: "Error occurred!",
-                    error: err
-                });
+                if (err.notFound) return callback("User does not exist.", false);
+                else return callback("Error occurred!", false);
             }
 
-            if (value == credentials.password) return reply({
-                result: 0,
-                message: "Login successful."
-            });
-            else return reply({
-                result: 1,
-                message: "Login failed. Incorrect password."
-            });
+            if (value == password) return callback(null, true);
+            else return callback("Login failed. Incorrect password.", false);
         });
     }
 }
